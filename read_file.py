28e5fdb669a708d1f3731c4cc0776884568d117b
rtmcmill <rtmcmill@ncsu.edu>
2015-10-03 16:05:44 -0400

    This is a push of a python and txt file.  The python file will open and modify the contexts of test_file.txt

---
+++ read_file.py
@@ -0,0 +1,15 @@
+import os
+
+
+print "This is your working directory: ",os.getcwd()
+
+answer = input("\n\r1) Write to file\n\r2) Read from file\n\r3) Exit\r\nChoose what you want to do:")
+
+if answer == 1:
+	pass
+
+if answer == 2:
+	pass 
+
+if answer == 3:
+	pass
